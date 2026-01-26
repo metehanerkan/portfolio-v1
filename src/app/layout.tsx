6c18344a09123becb6b6816cc4d463da331ff87e
metehanerkan <metehanerkan08@gmail.com>
2026-01-26 15:55:06 +0300

    Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
@@ -28,7 +30,29 @@ export default function RootLayout({
 }) {
     return (
         <html lang="tr">
+            <head>
+                {/* Google Tag Manager - HEAD */}
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-T28PX6KH');
+                    `}
+                </Script>
+            </head>
             <body className={`${inter.className} bg-[#030014]`}>
+                {/* Google Tag Manager (noscript) - BODY */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-T28PX6KH"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
+
                 <Toaster position="top-center" toastOptions={{ duration: 3000, style: { background: '#333', color: '#fff' } }} />
 
                 {children}
